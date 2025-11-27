ef6fded65fae69ee162ace8790a11f2dc97bf68f
santola.developer@gmail.com <santola.developer@gmail.com>
2025-11-27 07:06:36 +0700

    add folder config for store Dbconnnectio class global static

---
+++ charity-system/CurrentUser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace charity_system
+{
+    public static  class CurrentUser
+    {
+        public static int UserID { get; set; }
+        public static int RoleID { get; set; }
+        public static int? DonorID { get; set; }
+    }
+}
---
+++ charity-system/config/DBConnection.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace charity_system.config
+{
+    public static  class DBConnection
+    {
+        // Global database connection string
+        public static string ConnectionString = @"Data Source=DESKTOP-LC5ON52\SQLEXPRESS;Initial Catalog=CharityDB;Integrated Security=True;";
+    }
+}
